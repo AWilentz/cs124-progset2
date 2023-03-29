fc454a9e252d75629d507f2bfc3279358951fa4a
Alex Wilentz <awilentz@college.harvard.edu>
2023-03-29 04:12:39 -0400

    fully implemented strassen's and triangles in random graphs, ready to be tested

--- progset2.py
+++ progset2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def matrix(m1, m2):
 
     r1 = len(m1)
@@ -24,6 +26,8 @@ def strassen(m1, m2, n0):
     r2 = len(m2)
     c2 = len(m2[0])
 
+    padded = False
+
     n = r1
 
     if r2 != r1 or c1 != r1 or c2 != r1:
@@ -34,6 +38,15 @@ def strassen(m1, m2, n0):
     
     if n == 1:
         return [[m1[0][0] * [0][0]]]
+    
+    if not power_of_two(n):
+        n_ = n
+        n = 1<<(n-1).bit_length()
+        padded = True
+        m1 = np.vstack((m1, [[0 for _ in range(n_)] for _ in range(n-n_)]))  
+        m1 = np.hstack((m1, np.transpose([[0 for _ in range(n)] for _ in range(n-n_)])))
+        m2 = np.vstack((m2, [[0 for _ in range(n_)] for _ in range(n-n_)]))  
+        m2 = np.hstack((m2, np.transpose([[0 for _ in range(n)] for _ in range(n-n_)])))
     
     half = n // 2
 
@@ -53,7 +66,7 @@ def strassen(m1, m2, n0):
     bottom_left = add(p3, p4)
     bottom_right = subtract(subtract(add(p5, p1), p3), p7)
 
-    m = [[0 for _ in range(r1)] for _ in range(r1)]
+    m = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(half):
         for j in range(half):
             m[i][j] = top_left[i][j]
@@ -61,10 +74,18 @@ def strassen(m1, m2, n0):
             m[i + half][j] = bottom_left[i][j]
             m[i + half][j + half] = bottom_right[i][j]
 
+    if padded:
+        m = [[m[i][j] for j in range(n_)] for i in range(n_)]
+
     return m
 
+def power_of_two(n):
+    if(n & n-1 == 0 and n > 0):
+        return True
+    return False
+
 def split(m, half):
-    return [row[:half] for row in m[:half]], [row[half:] for row in m[:half]], [row[:half] for row in m[half:]], [row[half:] for row in m[half:]]
+    return [r[:half] for r in m[:half]], [r[half:] for r in m[:half]], [r[:half] for r in m[half:]], [r[half:] for r in m[half:]]
 
 def add(m1, m2):
     m = [[0 for _ in range(len(m1[0]))] for _ in range(len(m1))]
@@ -84,7 +105,34 @@ def subtract(m1, m2):
 
     return m
 
-m1 = [[1, 2], [3, 4]]
-m2 = [[3, 4], [1, 2]]
+m1 = [[1, 2, 3], 
+      [3, 4, 5], 
+      [6, 7, 9]]
+
+m2 = [[3, 4, 5], 
+      [1, 2, 3], 
+      [6, 7, 9]]
 
 print(strassen(m1, m2, 1))
+
+def count_triangles(m):
+    A3 = strassen(m, strassen(m, m, 20), 20)
+
+    trace = np.trace(A3)
+
+    triangles = trace / 6
+
+    return triangles
+
+def random_graph(n, p):
+    m = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1, n):
+            r = np.random.rand()
+            if r <= p:
+                m[i][j] = 1
+                m[j][i] = 1
+    return m
+
+for p in [.01, .02, .03, .04, .05]:
+    print(count_triangles(random_graph(1024, p)))
